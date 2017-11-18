57c096fa9d197c448a04be7deb7866d6be29715a
Victor Feng <victor.feng@skytv.co.nz>
2017-11-18 22:43:18 +1300

    Add transitions to the job.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SpringBatchCourse
 This is the codebase for the course of Spring Batch
+
+## 作业流跳转
--- src/main/java/io/csdn/batchdemo/BatchConfig.java
+++ src/main/java/io/csdn/batchdemo/BatchConfig.java
@@ -1,5 +1,6 @@
 package io.csdn.batchdemo;
 
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -22,12 +23,39 @@ public class BatchConfig {
 
     @Bean public Step step1() {
         return this.stepBuilderFactory.get("step1").tasklet((stepContribution, chunkContext) -> {
-            System.out.println("Hello Spring Batch");
+            System.out.println("步骤1");
+            return RepeatStatus.FINISHED;
+        }).build();
+    }
+
+    @Bean public Step step2() {
+        return this.stepBuilderFactory.get("step2").tasklet((stepContribution, chunkContext) -> {
+            System.out.println("步骤2");
+            return RepeatStatus.FINISHED;
+        }).build();
+    }
+
+    @Bean public Step step3() {
+        return this.stepBuilderFactory.get("step3").tasklet((stepContribution, chunkContext) -> {
+//            stepContribution.setExitStatus(ExitStatus.FAILED);
+            System.out.println("步骤3");
+
             return RepeatStatus.FINISHED;
         }).build();
     }
 
     @Bean public Job job1() {
-        return this.jobBuilderFactory.get("job1").start(step1()).build();
+        return this.jobBuilderFactory.get("job1")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .build();
+    }
+
+    @Bean public Job transitionJob() {
+        return this.jobBuilderFactory.get("transitionJob")
+                .start(step1()).on(ExitStatus.COMPLETED.getExitCode()).to(step2())
+                .from(step2()).on(ExitStatus.COMPLETED.getExitCode()).to(step3())
+                .end().build();
     }
 }
