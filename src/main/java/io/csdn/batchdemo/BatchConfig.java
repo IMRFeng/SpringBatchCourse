5b7f7fbf77375acdde8e3355d4c5eec470e43c95
Victor Feng <victor.feng@skytv.co.nz>
2017-11-19 20:41:46 +1300

    Add split in.

--- src/main/java/io/csdn/batchdemo/BatchConfig.java
+++ src/main/java/io/csdn/batchdemo/BatchConfig.java
@@ -7,9 +7,12 @@ import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 @Configuration
 public class BatchConfig {
@@ -23,46 +26,49 @@ public class BatchConfig {
         this.jobBuilderFactory = jobBuilderFactory;
     }
 
+    @Bean public Tasklet tasklet() {
+        return (step, chunk) -> {
+            System.out.println("步骤名：" + chunk.getStepContext().getStepName()
+                    + ", 线程：" + Thread.currentThread().getName());
+            return RepeatStatus.FINISHED;
+        };
+    }
+
     @Bean public Step step1() {
         return this.stepBuilderFactory.get("step1").tasklet((stepContribution, chunkContext) -> {
-            System.out.println("步骤1 - 自定义流Flow");
+            System.out.println("步骤1 - 拆分及并行处理Flow, 步骤名：" + chunkContext.getStepContext().getStepName()
+                    + ", 线程：" + Thread.currentThread().getName());
             return RepeatStatus.FINISHED;
         }).build();
     }
 
     @Bean public Step step2() {
-        return this.stepBuilderFactory.get("step2").tasklet((stepContribution, chunkContext) -> {
-            System.out.println("步骤2");
-            return RepeatStatus.FINISHED;
-        }).allowStartIfComplete(true).build();
+        return this.stepBuilderFactory.get("step2")
+                .tasklet(tasklet()).build();
     }
 
     @Bean public Step step3() {
-        return this.stepBuilderFactory.get("step3").tasklet((stepContribution, chunkContext) -> {
-            System.out.println("步骤3");
-            return RepeatStatus.FINISHED;
-        }).build();
+        return this.stepBuilderFactory.get("step3")
+                .tasklet(tasklet()).build();
     }
 
     @Bean public Flow flow1() {
         return new FlowBuilder<Flow>("flow1")
-                .start(step1())
-                .next(step2())
+                .start(step2())
                 .build();
     }
 
-    @Bean public Job flow1Job() {
-        return this.jobBuilderFactory.get("flow1Job")
-                .incrementer(new RunIdIncrementer())
-                .start(flow1())
-                .next(step3())
-                .end().build();
+    @Bean public Flow flow2() {
+        return new FlowBuilder<Flow>("flow2")
+                .start(step3()).end();
     }
 
-    @Bean public Job flow2Job() {
-        return this.jobBuilderFactory.get("flow2Job")
+    @Bean public Job splitJob() {
+        return this.jobBuilderFactory.get("splitJob")
                 .incrementer(new RunIdIncrementer())
-                .start(step3()).on(ExitStatus.COMPLETED.getExitCode()).to(flow1()) //这里是无法使用.next执行flow
+                .start(step1())
+                .split(new SimpleAsyncTaskExecutor())
+                .add(flow1(), flow2())
                 .end().build();
     }
 }
