183677e749eaa605f2a3460c7b9bd4e0daa0c398
Victor Feng <victor.feng@skytv.co.nz>
2017-11-19 20:30:10 +1300

    Add Flow in.

--- src/main/java/io/csdn/batchdemo/BatchConfig.java
+++ src/main/java/io/csdn/batchdemo/BatchConfig.java
@@ -3,7 +3,10 @@ package io.csdn.batchdemo;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,34 +25,44 @@ public class BatchConfig {
 
     @Bean public Step step1() {
         return this.stepBuilderFactory.get("step1").tasklet((stepContribution, chunkContext) -> {
-            System.out.println("通过自定义决策者Decider在步骤或流之间进行动态跳转");
+            System.out.println("步骤1 - 自定义流Flow");
             return RepeatStatus.FINISHED;
         }).build();
     }
 
     @Bean public Step step2() {
         return this.stepBuilderFactory.get("step2").tasklet((stepContribution, chunkContext) -> {
-            System.out.println("返回了FALSE");
+            System.out.println("步骤2");
             return RepeatStatus.FINISHED;
         }).allowStartIfComplete(true).build();
     }
 
     @Bean public Step step3() {
         return this.stepBuilderFactory.get("step3").tasklet((stepContribution, chunkContext) -> {
-            System.out.println("返回了TRUE");
+            System.out.println("步骤3");
             return RepeatStatus.FINISHED;
         }).build();
     }
 
-    @Bean public JobExecutionDecider getDecider() {
-        return new StepBooleanDecider(true);
+    @Bean public Flow flow1() {
+        return new FlowBuilder<Flow>("flow1")
+                .start(step1())
+                .next(step2())
+                .build();
     }
 
-    @Bean public Job dynamicDecisionStepJob() {
-        return this.jobBuilderFactory.get("dynamicDecisionStepJob")
-                .start(step1()).on(ExitStatus.COMPLETED.getExitCode()).to(getDecider())
-                .from(getDecider()).on("FALSE").to(step2())
-                .from(getDecider()).on("TRUE").to(step3())
+    @Bean public Job flow1Job() {
+        return this.jobBuilderFactory.get("flow1Job")
+                .incrementer(new RunIdIncrementer())
+                .start(flow1())
+                .next(step3())
+                .end().build();
+    }
+
+    @Bean public Job flow2Job() {
+        return this.jobBuilderFactory.get("flow2Job")
+                .incrementer(new RunIdIncrementer())
+                .start(step3()).on(ExitStatus.COMPLETED.getExitCode()).to(flow1()) //这里是无法使用.next执行flow
                 .end().build();
     }
 }
