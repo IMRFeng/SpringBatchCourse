0c2ed68150bc6d999689319c1ece146cf874522e
Victor Feng <victor.feng@skytv.co.nz>
2017-12-02 21:59:53 +1300

    Add read csv functionality.

--- src/main/java/io/csdn/batchdemo/BatchConfig.java
+++ src/main/java/io/csdn/batchdemo/BatchConfig.java
@@ -1,16 +1,23 @@
 package io.csdn.batchdemo;
 
-import io.csdn.batchdemo.reader.DemoInputReader;
+import io.csdn.batchdemo.model.Customer;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.batch.item.file.transform.LineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.validation.BindException;
 
 @Configuration
 public class BatchConfig {
@@ -25,22 +32,67 @@ public class BatchConfig {
         this.jobBuilderFactory = jobBuilderFactory;
     }
 
-    @Bean public DemoInputReader readItems() {
-        return new DemoInputReader(Arrays.asList("A", "B", "C", "D"));
+    @Bean public Job csvFileReaderJob() {
+        return this.jobBuilderFactory.get("csvFileReaderJob")
+                .start(chunkBasedStep())
+                .build();
     }
 
     @Bean public Step chunkBasedStep() {
         return this.stepBuilderFactory.get("chunkBasedStep")
-                .<String, String>chunk(1)
-                .reader(readItems())
+                .<Customer, Customer>chunk(5)
+                .reader(csvFileItemReader())
                 .writer(list -> list.forEach(System.out::println))
-//                .allowStartIfComplete(true)
+                .allowStartIfComplete(true)
                 .build();
     }
 
-    @Bean public Job itemReaderJob() {
-        return this.jobBuilderFactory.get("itemReaderJob")
-                .start(chunkBasedStep())
-                .build();
+    @Bean public ItemReader<Customer> csvFileItemReader() {
+        FlatFileItemReader<Customer> reader = new FlatFileItemReader<>();
+
+        reader.setResource(new ClassPathResource("/data/us-500.csv"));
+        reader.setLinesToSkip(1);
+        reader.setLineMapper(this.createCustomerLineMapper());
+
+        return reader;
+    }
+
+    private LineMapper<Customer> createCustomerLineMapper() {
+        DefaultLineMapper<Customer> customerLineMapper = new DefaultLineMapper<>();
+
+        customerLineMapper.setLineTokenizer(this.createCustomerLineTokenizer());
+        customerLineMapper.setFieldSetMapper(new CustomerFieldSetMapper());
+//        customerLineMapper.setFieldSetMapper(this.createCustomerFieldSetMapper());
+        customerLineMapper.afterPropertiesSet();
+
+        return customerLineMapper;
+    }
+
+    private LineTokenizer createCustomerLineTokenizer() {
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+        tokenizer.setNames(new String[] {"first_name", "last_name", "company_name", "address", "city", "country",
+                "state", "zip", "phone1", "phone2", "email", "web"});
+        return tokenizer;
+    }
+
+    private FieldSetMapper<Customer> createCustomerFieldSetMapper() {
+        BeanWrapperFieldSetMapper<Customer> customerFieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        customerFieldSetMapper.setTargetType(Customer.class);
+        return customerFieldSetMapper;
+    }
+
+    /**
+     * 通过Customer的构造器来创建其对象，通常用于平面文件里的列名与对象属性不相同的情况
+     */
+    private static class CustomerFieldSetMapper implements FieldSetMapper<Customer> {
+
+        @Override
+        public Customer mapFieldSet(FieldSet fieldSet) throws BindException {
+            return new Customer(fieldSet.readString("first_name"), fieldSet.readString("last_name"),
+                    fieldSet.readString("company_name"), fieldSet.readString("address"),
+                    fieldSet.readString("city"), fieldSet.readString("country"), fieldSet.readString("state"),
+                    fieldSet.readString("zip"), fieldSet.readString("phone1"), fieldSet.readString("phone2"),
+                    fieldSet.readString("email"), fieldSet.readString("web"));
+        }
     }
 }
---
+++ src/main/java/io/csdn/batchdemo/model/Customer.java
@@ -0,0 +1,28 @@
+package io.csdn.batchdemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author Zhantao Feng.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer implements Serializable {
+    private String firstName;
+    private String lastName;
+    private String companyName;
+    private String address;
+    private String city;
+    private String country;
+    private String state;
+    private String zip;
+    private String phone1;
+    private String phone2;
+    private String email;
+    private String webUrl;
+}
